df0b4c1c4fb68fb4a7c647ef8a01b2f6deda2269
ttakayanagi3 <ttakayanagi3@gateh.edu>
2023-10-02 21:44:08 +0900

    implementing trainer

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import os
 import argparse
 
 import torch
+import torch.nn as nn
 from torchvision import datasets as ds
 from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
 
 import preprocess as p
 import models as m
 import metrics as me
+import trainer as t
+
 
 def load_mnist(opt, train_dype='train', base_dir='./data', need_sampling=True,
                data_size=1000):
@@ -60,13 +64,14 @@ def main():
     parser.add_argument("--FPS", type=int, default=FPS)
     parser.add_argument("--LENGTH_SEC", type=int, default=LENGTH_SEC)
     parser.add_argument("--batch_size", type=int, default=4, help="size of mini-batches")
+    parser.add_argument("--n_epochs", type=int, default=10, help="epochs")
     parser.add_argument("--NUM_SELF_CON_SIMPER", type=int, default=10)
     parser.add_argument("--MAX_SPEED", type=int, default=MAX_SPEED)
     parser.add_argument("--SSL_FRAMES", type=int, default=NUM_FRAMES // MAX_SPEED)
     parser.add_argument("--IMG_SIZE", type=int, default=IMG_SIZE)
     parser.add_argument("--CHANNELS", type=int, default=CHANNELS)
     parser.add_argument("--lr", type=float, default=1e-3, help="Learning Rate")
-    parser.add_argument("--DEBUG", type=int, default=1)
+    parser.add_argument("--DEBUG", type=int, default=0)
     #
     #
     parser.add_argument("--label_dist_fn", type=str, default='l1')
@@ -78,7 +83,7 @@ def main():
 
     train_data, train_label, train_freq = load_mnist(opt, train_dype='train')
     dataset = p.CustomDataset(train_data, train_label, train_freq, opt)
-    dataloader = DataLoader(dataset=dataset, batch_size=opt.batch_size)
+    data_loader = DataLoader(dataset=dataset, batch_size=opt.batch_size)
     model = m.SimPer(opt)
     if opt.DEBUG == 1:
         data_iter = iter(dataloader)
@@ -110,9 +115,10 @@ def main():
             if opt.feat_dist_fn == 'max_corr':
                 feat_dist = me.max_cross_corr(feat1, feat2)
 
-
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+    t.train(data_loader, model, criterion, optimizer, opt)
+
 
 
     print('complete')
--- trainer.py
+++ trainer.py
@@ -1,9 +1,66 @@
+import tqdm
 import torch
 import torch.nn as nn
 
+import preprocess as p
+import models as m
+import metrics as me
+import trainer as t
 
-def trainer(data_loader, model, optimizer, opt):
-    for i in range(opt.n_epochs):
-        for x, y_label, y_angle in data_loader:
-            out = model(x)
 
+def save_params(epoch, file_path, model):
+    file_name = "model_{:04d}.pth".format(epoch)
+    if not os.path.exists(file_path):
+        os.makedirs(file_path, exist_ok=True)
+    file_path_ = os.path.join(file_path, file_name)
+    torch.save(
+        model.state_dict(),
+        file_path_
+    )
+
+
+def train(data_loader, model, criterion, optimizer, opt):
+    losses = []
+    for epoch in tqdm.tqdm(range(opt.n_epochs)):
+        running_loss = 0
+        for frames, all_speed, y_angle in data_loader:
+            #
+            # split all speed
+            # 2 * M -> M, M
+            #
+            num_arguments = frames.shape[1]
+            half_of_num_arguments = int(num_arguments // 2)
+            all_speed1 = all_speed[:, :half_of_num_arguments]
+            all_speed2 = all_speed[:, half_of_num_arguments:]
+            #
+            # label distance
+            #
+            all_labels = me.label_distance(all_speed1, all_speed2,
+                                           opt.label_dist_fn, opt.label_temperature)
+            batch_size = frames.shape[0]
+            shape = frames.shape[2:]
+            transform_shape = (batch_size * num_arguments, *shape)
+            frames_transformed = frames.view(transform_shape)
+            #
+            # inference
+            #
+            model.zero_grad()
+            all_z = model(frames_transformed, 'f')
+            all_z = all_z.view(batch_size, num_arguments, -1)
+
+            loss = 0
+            for feats, labels in zip(all_z, all_labels):
+                # print(feats, labels)
+                feat1 = feats[:half_of_num_arguments]
+                feat2 = feats[half_of_num_arguments:]
+                if opt.feat_dist_fn == 'max_corr':
+                    feat_dist = me.max_cross_corr(feat1, feat2)
+                else:
+                    pass
+                loss += criterion(feat_dist, labels)
+            loss /= batch_size
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        losses.append(running_loss)
+        save_params(epoch, 'params', model)
